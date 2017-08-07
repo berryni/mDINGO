91e642f8fdb9ae904d6b3dab2dff71893d5693e4
NICHOLAS BERRY <Nick@NICHOLASs-MacBook-Pro-4.local>
2017-08-07 01:18:23 -0500

    Updated with graphList2precs function to create global precision, and then update control and disease precisions afterwards. Also fixed bug in igraph2prec.

--- R/SpiecEasiRewrites.R
+++ R/SpiecEasiRewrites.R
@@ -1,5 +1,76 @@
 ## SPiecEasi rewritten or copied functions because they werent working for me, or I needed a workaround.
 
+graphList2precs = function(graphList, posThetaLims = c(2, 3), negThetaLims = -posThetaLims, 
+                           targetCondition = 100, epsBin = 0.01, numBinSearch = 100)
+{
+  posThetaLims <- sort(posThetaLims)
+  negThetaLims <- sort(negThetaLims)
+  global = as.matrix(graphList$global[])
+  n = ncol(global)
+  Theta <- global + diag(n)
+  degVec <- colSums(global)
+  utri <- SpiecEasi::triu(Theta)
+  nzind <- which(utri != 0)
+  if (length(posThetaLims) > 2 || length(negThetaLims) > 2) 
+    stop("theta_max and theta_min should be a numeric vector of length 1 or 2")
+  rands <- runif(length(nzind))
+  mapToRange <- function(x, lim) {
+    span <- diff(sort(lim))
+    min(lim) + (x * span)
+  }
+  boolind <- sample(c(TRUE, FALSE), length(nzind), replace = TRUE)
+  rands[boolind] <- mapToRange(rands[boolind], posThetaLims)
+  rands[!boolind] <- mapToRange(rands[!boolind], negThetaLims)
+  utri[nzind] <- rands
+  #####Theta <- triu2diag(utri, 1) <- this wasn't working. The next 2 lines do what the function did I think.
+  Theta[upper.tri(Theta)] = utri
+  Theta[lower.tri(Theta)] = t(Theta)[lower.tri(Theta)]
+  diag(Theta) = 1
+  
+  disease = as.matrix(graphList$disease[])
+  control = as.matrix(graphList$control[])
+  disTheta = disease + diag(n)
+  conTheta = control + diag(n)
+  disutri <- SpiecEasi::triu(disTheta)
+  disutri[utri != 0] = utri[utri != 0]
+  disnzind <- which(utri == 0 & disutri != 0)
+  conutri <- SpiecEasi::triu(conTheta)
+  conutri[utri != 0] = utri[utri != 0]
+  connzind <- which(utri == 0 & conutri != 0)
+  rands <- runif(length(disnzind) + length(connzind))
+  boolind <- sample(c(TRUE, FALSE), length(disnzind) + length(connzind), replace = TRUE)
+  rands[boolind] <- mapToRange(rands[boolind], posThetaLims)
+  rands[!boolind] <- mapToRange(rands[!boolind], negThetaLims)
+  disutri[disnzind] = rands[1:length(disnzind)]
+  conutri[connzind] = rands[(length(disnzind)+1):(length(disnzind)+length(connzind))]
+  
+  disTheta[upper.tri(disTheta)] = disutri
+  disTheta[lower.tri(disTheta)] = t(disTheta)[lower.tri(disTheta)]
+  diag(disTheta) = 1
+  conTheta[upper.tri(conTheta)] = conutri
+  conTheta[lower.tri(conTheta)] = t(conTheta)[lower.tri(conTheta)]
+  diag(conTheta) = 1
+  
+  eigVals <- eigen(disTheta)$values
+  minEig <- min(eigVals)
+  maxEig <- max(eigVals)
+  if (minEig < 0.01) 
+    disTheta <- disTheta + abs(minEig) * diag(n)
+  disdiagConst <- .binSearchCond(disTheta, targetCondition, numBinSearch, 
+                              epsBin)
+  eigVals <- eigen(conTheta)$values
+  minEig <- min(eigVals)
+  maxEig <- max(eigVals)
+  if (minEig < 0.01) 
+    conTheta <- conTheta + abs(minEig) * diag(n)
+  condiagConst <- .binSearchCond(conTheta, targetCondition, numBinSearch, 
+                                 epsBin)
+  
+  disTheta = disTheta + max(disdiagConst, condiagConst) * diag(n)
+  conTheta = conTheta + max(disdiagConst, condiagConst) * diag(n)
+  return(list(disTheta, conTheta))
+}
+
 igraph2prec = function (Graph, posThetaLims = c(2, 3), negThetaLims = -posThetaLims, 
                         targetCondition = 100, epsBin = 0.01, numBinSearch = 100) 
 {
@@ -25,6 +96,7 @@ igraph2prec = function (Graph, posThetaLims = c(2, 3), negThetaLims = -posThetaL
   rands[!boolind] <- mapToRange(rands[!boolind], negThetaLims)
   utri[nzind] <- rands
   #####Theta <- triu2diag(utri, 1) <- this wasn't working. The next 2 lines do what the function did I think.
+  Theta[upper.tri(Theta)] = utri
   Theta[lower.tri(Theta)] = t(Theta)[lower.tri(Theta)]
   diag(Theta) = 1
   
@@ -33,8 +105,7 @@ igraph2prec = function (Graph, posThetaLims = c(2, 3), negThetaLims = -posThetaL
   maxEig <- max(eigVals)
   if (minEig < 0.01) 
     Theta <- Theta + abs(minEig) * diag(n)
-  diagConst <- .binSearchCond(Theta, targetCondition, numBinSearch, 
-                              epsBin)
+  diagConst <- .binSearchCond(Theta, targetCondition, numBinSearch, epsBin)
   Theta <- Theta + diagConst * diag(n)
   return(Theta)
 }
